85a3983691283aee170c3d20315d165a1c616aa7
Hidetoshi Akaike <repon27@yahoo.co.jp>
2023-07-14 18:34:44 +0900

    複数のstoryを記述、setup関数をmetaに移動

--- src/components/MyButton.stories.ts
+++ src/components/MyButton.stories.ts
@@ -6,13 +6,33 @@ type Story = StoryObj<typeof MyButton>;
 const meta: Meta<typeof MyButton> = {
   title: "MyButton",
   component: MyButton,
+  render: (args) => ({
+    components: { MyButton },
+    setup() {
+      return { args };
+    },
+    template: "<MyButton v-bind='args' />",
+  }),
 }
 
+// 「ボタン」
 export const Default: Story = {
-  render: () => ({
-    components: { MyButton },
-    template: "<MyButton label='ボタン' />",
-  }),
+  args: {
+    label: "ボタン",
+  },
+}
+
+// 「ログイン」
+export const Login: Story = {
+  args: {
+    label: "ログイン",
+  },
+}
+
+export const SignUp: Story = {
+  args: {
+    label: "会員登録",
+  }
 }
 
 export default meta
